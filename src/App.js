6c8391519286b60ed3df3dcb34951f106857f538
chlwlgus4 <chlwlgus89@naver.com>
2021-03-28 22:48:35 +0900

    렌더링 최적화 추가

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
+function createBulkTodos() {
+    const array = [];
+    for (let i = 0; i <= 2500; i++) {
+        array.push({
+            id: i,
+            text: `할 일 ${i}`,
+            checked: false,
+        });
+    }
+    return array;
+}
+
 const App = () => {
 
-    const [todos, setTodos] = useState([
+    const [todos, setTodos] = useState(
+    /*    [
         {
             id: 1,
             text: '리액트의 기초 알아보기',
@@ -21,11 +34,14 @@ const App = () => {
             text: '일정 관리 앱 만들어 보기',
             checked: false,
         },
-    ]);
+    ]*/
+        createBulkTodos
+    );
 
     // 고윳값으로 사용될 id
     // ref를 사용하여 변수 담기
-    const nextId = useRef(4);
+    // const nextId = useRef(4);
+    const nextId = useRef(2501);
 
     //todos값이 변할 때 마다 렌더링이 되기 때문에 최적화를 위해 useCallback사용
     const onInsert = useCallback(
@@ -35,27 +51,42 @@ const App = () => {
                 text,
                 checked: false,
             }
-            setTodos(todos.concat(todo));
+            // setTodos(todos.concat(todo));
+            // 최적화
+            setTodos(todo => todo.concat(todo));
             nextId.current += 1; //nextId 1씩 증가
         },
-        [todos],
+        // [todos],
+        // 최적화
+        [],
     );
 
     const onRemove = useCallback(
         id => {
-            setTodos(todos.filter(todo => todo.id !== id));
+            // setTodos(todos.filter(todo => todo.id !== id));
+            //최적화
+            setTodos(todos =>  todos.filter(todo => todo.id !== id));
         },
-        [todos]
+        // [todos]
+        // 최적화
+        []
     );
 
     const onToggle = useCallback(
         id => {
-            setTodos(
-                todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked } : todo,
+            // setTodos(
+            //     todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked } : todo,
+            //     ),
+            //최적화
+            setTodos( todos =>
+                todos.map(todo =>
+                    todo.id === id ? {...todo, checked: !todo.checked } : todo,
                 ),
             )
         },
-        [todos]
+        // [todos]
+        // 최적화
+        []
     )
 
     return (
